4b2022b748cf5feceb1afd9097fc1e37d6fb1520
Régis Gaidot <rgaidot@gmail.com>
2026-08-28 21:24:43 +0200

    feat(date): drop the time from displayed dates

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,10 +1,6 @@
 import { format } from "date-fns";
 import { fr } from "date-fns/locale";
 
-export function formatFrenchDate(date: Date): string {
-  return format(date, "EEEE d MMMM yyyy à HH:mm", { locale: fr });
-}
-
 export function formatFrenchDateShort(date: Date): string {
   return format(date, "d MMMM yyyy", { locale: fr });
 }
